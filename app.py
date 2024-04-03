900dc5384cf64c13cfd8b5ccc92ae226e8d3f836
Paulo2991 <paulovaz38034@gmail.com>
2024-04-03 13:08:30 -0300

    Ajustando a operação do sqlite

--- app.py
+++ app.py
@@ -1,43 +1,132 @@
-from flask import Flask, render_template, request, redirect, url_for
+import sqlite3
+from flask import Flask, request, g
 
-app = Flask(__name__, template_folder='templates')
 
-# Lista para armazenar os dados das pessoas cadastradas
-pessoas = []
+DATABASE = 'pessoa.db'
 
-# Rota para exibir a página inicial com a lista de pessoas cadastradas e opções CRUD
-@app.route('/')
-def index():
-    return render_template('index.html', pessoas=pessoas)
 
-# Rota para cadastrar uma nova pessoa
-@app.route('/cadastrar', methods=['POST'])
-def cadastrar_pessoa():
-    nome = request.form['nome']
-    cpf = request.form['cpf']
-    pessoas.append({'nome': nome, 'cpf': cpf})
-    return redirect(url_for('index'))
+app = Flask(__name__)
+app.json.sort_keys = False
 
-# Rota para editar uma pessoa
-@app.route('/editar/<cpf>', methods=['GET', 'POST'])
-def editar_pessoa(cpf):
-    for pessoa in pessoas:
-        if pessoa['cpf'] == cpf:
-            if request.method == 'POST':
-                pessoa['nome'] = request.form['nome']
-                pessoa['cpf'] = request.form['cpf']
-                return redirect(url_for('index'))
-            return render_template('editar.html', pessoa=pessoa)
-    return "Pessoa não encontrada."
 
-# Rota para excluir uma pessoa
-@app.route('/excluir/<cpf>')
-def excluir_pessoa(cpf):
-    for pessoa in pessoas:
-        if pessoa['cpf'] == cpf:
-            pessoas.remove(pessoa)
-            return redirect(url_for('index'))
-    return "Pessoa não encontrada."
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
+def init_db():
+    with app.app_context():
+        sql_pragma_foreign_keys = "PRAGMA foreign_keys = ON;"
+        get_db().cursor().execute(sql_pragma_foreign_keys)
+
+        sql_create_departments_table = """
+        CREATE TABLE IF NOT EXISTS pessoa (
+            id INTEGER PRIMARY KEY NOT NULL UNIQUE,
+            nome TEXT NOT NULL,
+            cpf TEXT NOT NULL
+        );
+        """
+        get_db().cursor().execute(sql_create_departments_table)
+
+
+        get_db().commit()
+
+
+@app.route("/pessoas", methods=["POST"])
+def cadastrarPessoas():
+    data = request.get_json()
+
+    cursor = get_db().cursor()
+    sqlInsertPessoas = "INSERT INTO pessoa (nome, cpf) VALUES (?, ?);"
+    cursor.execute(sqlInsertPessoas, (data["nome"], data["cpf"]))
+    get_db().commit()
+
+    return {
+        "id": cursor.lastrowid,
+        "nome": data["nome"],
+        "cpf": data["cpf"]    
+        }
+
+
+@app.route("/", methods=["GET"])
+def listarPessoas():
+    sqlSelectPessoas = "SELECT * FROM pessoa;"
+    rows = get_db().cursor().execute(sqlSelectPessoas).fetchall()
+
+    pessoas = []
+    for row in rows:
+        pessoa = {
+            "id": row[0],
+            "nome": row[1],
+            "cpf": row[2]
+        }
+        pessoas.append(pessoa)
+
+    return pessoas
+
+
+@app.route("/pessoas/<int:id>", methods=["GET"])
+def listarPessoasId(id):
+    sqlSelectPessoasId = "SELECT * FROM pessoa WHERE id = ?;"
+    row = get_db().cursor().execute(sqlSelectPessoasId, (id,)).fetchone()
+
+    if not row:
+        return {"message": "Department not found."}, 404
+
+    pessoaListada = {
+        "id": row[0],
+        "nome": row[1],
+        "cpf": row[2]    
+    }
+
+    return pessoaListada
+
+
+@app.route("/pessoas/<int:id>", methods=["PUT"])
+def editarPessoas(id):
+    sqlSelectEditarPessoas = "SELECT * FROM pessoa WHERE id = ?;"
+    row = get_db().cursor().execute(sqlSelectEditarPessoas, (id,)).fetchone()
+
+    if not row:
+        return {"message": "Department not found."}, 404
+
+    data = request.get_json()
+
+    if "nome" not in data or "cpf" not in data:
+        return {"message": "Missing fields."}, 400
+
+    sqlUpdatePessoas = "UPDATE pessoa SET nome = ?, cpf = ? WHERE id = ?;"
+    get_db().cursor().execute(
+        sqlUpdatePessoas,
+        (data["nome"], data["cpf"], id)
+    )
+    get_db().commit()
+
+    pessoa = {
+        "id": id,
+        "nome": data["nome"],
+        "cpf": data["cpf"]
+    }
+
+    return pessoa
+
+
+@app.route("/pessoas/<int:id>", methods=["DELETE"])
+def deletarPessoas(id):
+    sqlSelectDeletarPessoas = "SELECT * FROM pessoa WHERE id = ?;"
+    row = get_db().cursor().execute(sqlSelectDeletarPessoas, (id,)).fetchone()
+
+    if not row:
+        return {"message": "Department not found."}, 404
+
+    sqlDeletePessoas = "DELETE FROM pessoa WHERE id = ?;"
+    get_db().cursor().execute(sqlDeletePessoas, (id,))
+    get_db().commit()
+
+    return "", 204
+
+if __name__ == "__main__":
+    init_db()
+    app.run(host="0.0.0.0", port=5000, debug=True, use_reloader=True)
